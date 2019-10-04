771ab754b2ec946aae3848883980a85f09e20804
Gloriana Fernandez <glorianafernandezq@hotmail.com>
2019-10-04 12:41:07 -0600

    Nombres packages

--- src/casoiterator/IContainer.java
+++ src/casoiterator/IContainer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package iteratorpattern;
+package casoiterator;
 
 public interface IContainer<T> {       
     public IIterator<T> createIterator();   
--- src/casoiterator/IteratorMain.java
+++ src/casoiterator/IteratorMain.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package iteratorpattern;
+package casoiterator;
 
 public class IteratorMain {          
     public static void main(String[] args) {           
